5cd236a4588bc55fbd589ed1e7bdf2869bfed559
Joma Ejercito <jomaejercito@gmail.com>
2018-04-04 22:42:58 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -23,6 +23,7 @@ def can_be_created_with_a_hash_of_attributes
 end
 
 def can_be_created_in_a_block(args = { title: "Home Alone", release_date: 1990 })
+  binding.pry
   Movie.create do |m|
     m.title = title:
     m.release_date = release_date:
