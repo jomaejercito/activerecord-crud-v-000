955a6b4eea848723436d5ab52d3b8b84df41e270
Joma Ejercito <jomaejercito@gmail.com>
2018-04-04 22:35:52 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,8 +24,8 @@ end
 
 def can_be_created_in_a_block(args = :title)
   Movie.create do |m|
-    m.title = "Home Alone"
-    m.release_date = 1990
+    m.title = "The Room"
+    m.release_date = 2003
   end
 end
 
